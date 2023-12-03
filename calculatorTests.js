f137dfad8d9abd031592881ac03ef842770aea7c
DALI <sika.dali@telecomnancy.net>
2023-12-03 01:00:57 +0100

    TDD settings - test running + debug ok

--- calculatorTests.js
+++ calculatorTests.js
@@ -1,18 +1,25 @@
-import { describe, it } from 'mocha';
+import { describe, it, beforeEach } from 'mocha';
 import { expect } from 'chai';
 import { JSDOM } from 'jsdom';
 
-describe('updateMsg', function () {
+describe('Digit display', function () {
     var runs = [
-        {it: '0', option: 0}
+        {it: '0', option: 0}/*,
+        {it: '1', option: 1},
+        {it: '2', option: 2},
+        {it: '3', option: 3},
+        {it: '4', option: 4},
+        {it: '5', option: 5},
+        {it: '6', option: 6},
+        {it: '7', option: 7},
+        {it: '8', option: 8},
+        {it: '9', option: 9}*/
     ];
 
-    beforeEach(function() {
-      return JSDOM.fromFile('index.html')
-        .then((dom) => {
-          global.window = dom.window;
-          global.document = dom.window.document;
-        });
+    beforeEach(async function() {
+      const dom = await JSDOM.fromFile('index.html', { runScripts: "dangerously", resources: "usable" });
+        global.window = dom.window;
+        global.document = dom.window.document;
     })
     
     runs.forEach(function (run) {
@@ -24,7 +31,7 @@ describe('updateMsg', function () {
             digit.click();
 
             // assert
-            expect(document.getElementById('display').innerHTML).to.equal(''+run.option);
+            expect(document.getElementById('display').value).to.equal('' + run.option);
         });
     });
 });
